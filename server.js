8c67a3b3a39512a59eaecfac97fa21c4abf171ee
Szymon Warszawa <szymonwarszawa@gmail.com>
2021-03-08 14:52:11 +0000

    Create API router.

--- server.js
+++ server.js
@@ -4,13 +4,17 @@ const cors = require("cors");
 const errorHandler = require("errorhandler");
 const e = require("cors");
 
+const apiRouter = require("./api/api");
+
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(bodyParser.json());
 app.use(cors());
-app.use(errorHandler());
 app.use(morgan("dev"));
+
+app.use("/api", apiRouter);
+app.use(errorHandler());
 
 app.listen(PORT, () => {
   console.log(`Listening on port: ${port}`);
